fb36982f7b0d76be50b2f8a6fbf793022404a033
Israel Alagbe <israelalagbe53@gmail.com>
2021-07-10 12:48:27 +0100

    Store product request test

---
+++ tests/Unit/StoreProductRequestTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Http\Controllers\ProductRequestController;
+use App\Http\Requests\StoreProductRequest;
+use Tests\TestCase;
+
+class StoreProductRequestTest extends TestCase
+{
+    private StoreProductRequest $formRequest;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->formRequest = new StoreProductRequest();
+    }
+
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testValidationRules()
+    {
+        $this->assertEquals(
+            [
+                'title' => ['required', 'string', 'max:100'],
+                'description' => ['required', 'string', 'max:1000'],
+            ],
+            $this->formRequest->rules()
+        );
+    }
+    public function testUsageByAppropriateControllers()
+    {
+        $this->assertActionUsesFormRequest(
+            ProductRequestController::class,
+            'storeProductRequest',
+            get_class($this->formRequest)
+        );
+    }
+    public function testRequestAuthorization()
+    {
+        $this->assertTrue($this->formRequest->authorize());
+    }
+}
